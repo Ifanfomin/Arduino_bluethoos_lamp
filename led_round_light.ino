7882696d6031a42a3fa13d03fc628bbca41fad3d
Ifanfomin <129762316+Ifanfomin@users.noreply.github.com>
2023-07-28 14:09:26 +0300

    Влижение произвольной полоски по кругу

--- led_round_light.ino
+++ led_round_light.ino
@@ -3,19 +3,33 @@
 #include "FastLED.h"
 CRGB leds[NUM_LEDS];
 int led_num = 0;
+int value = 255;
+int len = 5; // длина крутящегося элемента
+int value_change = 0;
 
 void setup() {
-  FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS); //.setCorrection( TypicalLEDStrip )
+  FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
   pinMode(13, OUTPUT); // 6 вольт, 0,03 А
+  if (len == 0) {
+    value_change = value;
+  } else {
+    value_change = value / len;
+  }
 }
 
 void loop() {
   for (int i = 0; i < NUM_LEDS; i++){
     led_num = i;
+    value = 255;
     leds[led_num] = CHSV(127, 255, 255);
     FastLED.show();
-    delay(100);
-    leds[led_num--] = CHSV(127, 255, 0);
+    delay(80);
+    for (int j = 0; j < len; j++) {
+      if (led_num == -1) {
+        led_num = 7;
+      }
+      leds[led_num--] = CHSV(127, 255, value = value - value_change);
+    }
   }
 }
